48bbbef4c307a7654882cc28642111baa9ebbed2
samukelisiwem <samukelisiwe.msweli@mandela.ac.za>
2025-07-24 15:55:21 +0200

    update codes

--- Script/01msc_cederberg_CombinedFlamm.R
+++ Script/01msc_cederberg_CombinedFlamm.R
@@ -5,12 +5,16 @@ library(tidyverse)
 library(readxl) 
 library(GGally)
 library(hms)
+library(lme4)
+library(ggfortify)
+library(factoextra)
+
 
 #reads data
 #Loads the Excel file
 #Cleans and reshapes the data
-#Plots faceted box plots with rotated x-axis labels
-#flammability attributes varies across species
+#Plot faceted boxplots with rotated x-axis labels
+#species variation across flammability attributes 
 read_xlsx("Data/01msc_cederberg_CombinedFlamm.xlsx", sheet = "Combined") %>%
   pivot_longer(cols = c("TimeToFlaming", "PostBurntMassEstimate", "MaximumFlameTemperature","FMC_percentage"),
                names_to = "variable", values_to = "value") %>%
@@ -19,23 +23,30 @@ read_xlsx("Data/01msc_cederberg_CombinedFlamm.xlsx", sheet = "Combined") %>%
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6)) +
   facet_wrap(vars(variable), scales = "free")
 
+
 #create an object of the data I want to use
+#OG data as recorded in wide format
 CombinedOG <- read_xlsx("Data/01msc_cederberg_CombinedFlamm.xlsx", sheet = "Combined")
+
 
 #See what sheets are in the Excel workbook
 excel_sheets("Data/01msc_cederberg_CombinedFlamm.xlsx") #aaahh!
 
-#to see class category of table contents
-sapply(CombinedOG, class)            #melikeythisone
 
-#make Date column to Date class and timeburnt to HM
+#to see class category of OG data
+sapply(CombinedOG, class)                               #melikeythisone
+
+
+#Date is numeric and time burnt is 1899-12-31 10:10:00
+#correct to accepted formats
 CombinedOG <- CombinedOG %>%
   mutate(
     Date = as.Date(as.character(Date), format = "%Y%m%d"),
     `time burnt` = format(`time burnt`, "%H:%M")
   )
 
-#create Datasource shows data from PhD or MSc
+
+#create Datasource column to show if data from PhD/ MSc
 CombinedOG <- CombinedOG %>%
   mutate(
     year = substr(as.character(Date), 1, 4),
@@ -44,9 +55,11 @@ CombinedOG <- CombinedOG %>%
       year %in% c("2018", "2019") ~ "MSc")
     )
   
+
 #checking PhD/MSc entries
 CombinedOG %>%
-  count(DataSource)     #alrightee
+  count(DataSource)                           #alrightee
+
 
 #rename some headings
 CombinedOG <- CombinedOG %>%
@@ -57,51 +70,88 @@ CombinedOG <- CombinedOG %>%
     `MaximumFlameTemperature(°C)` = MaximumFlameTemperature
     )
 
-#see all columns in df 
+
+#see all columns in OG 
 colnames(CombinedOG)
 
-#remove columns not needed
+#remove columns not really needed
 CombinedOG <- CombinedOG %>%
-  select(-`PreFireMass_(g)`, -PBM, -MFT, -TTF, -year, -paperbagID)
+  select(-`PreFireMass_(g)`, -PBM, -MFT, -TTF, -year, -paperbagID, -`fresh mass (g)`, -`dried mass (g)`)
 
 summary(CombinedOG)
 
-#CombinedOG in long format 
+
+#Pivot CombinedOG to long format 
 CombinedLong <- CombinedOG %>%
   pivot_longer(cols = c("TimeToFlaming(s)", "PostBurntMassEstimate(%)", "MaximumFlameTemperature(°C)", "FMC(%)", "FlammabilityIndex"),
-               names_to = "variable", values_to = "value")
+               names_to = "flammtrait", values_to = "value")
+
 
 #boxplots on Long data
+#species across variables 
 CombinedLong %>%
-  filter(variable != "FlammabilityIndex") %>%
-  group_by(variable) %>%
+  filter(flammtrait != "FlammabilityIndex") %>%
+  group_by(flammtrait) %>%
   ggplot(aes(y = value, x = species_code, fill = DataSource)) +
   geom_boxplot() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 6)) +
-  facet_wrap(vars(variable), scales = "free")       #MSc has more shrubs and trees
-                                                    #PhD has more herbs and shrubs
+  facet_wrap(vars(flammtrait), scales = "free")                       #MSc has more shrubs and trees
+                                                                      #MSc has more higher temps compared to PhD  
+                                                                      #PhD has more herbs and shrubs
                                                    
 
-#see if MSc vs PhD independent datas if comparable
-CombinedLong %>%
-  group_by(variable) %>%
-  summarise(
-    p_value = wilcox.test(value ~ DataSource)$p.value
-  )
+#see if MSc vs PhD are independent / if comparable
+#sure differ by site, species, samples, replicates,...etc
+summary(lm(value ~ DataSource + species_name, data = CombinedLong))
+                                                                      #No significant difference in the values between MSc and PhD
+                                                                      #accoubts for species variation, this makes some sense to me than lmer below 
+                                                                      #here datas comparable
 
-#Flamm Attriutes by family
+
+#maybe treat species as a random factor? 
+summary(lmer(value ~ DataSource + (1 | species_name), data = CombinedLong))
+                                                                      #not sure about this one.data not comparable and species =no variation?
+
+
+#Flamm Attributes by family
 CombinedLong %>%
+  filter (flammtrait != "FlammabilityIndex") %>%
   ggplot() +
   geom_boxplot(aes(y = value, x = Accepted_family, fill = DataSource)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) +
-  facet_wrap(vars(variable), scales = "free")
+  facet_wrap(vars(flammtrait), scales = "free")
 
-#Flamm Attriutes by growth form
+
+#Flamm Attributes by growth form
 CombinedLong %>%
+  filter (flammtrait != "FlammabilityIndex") %>%
   ggplot() +
   geom_boxplot(aes(y = value, x = growth_form, fill = DataSource)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  facet_wrap(vars(variable), scales = "free")
+  facet_wrap(vars(flammtrait), scales = "free")
+
+
+##Does flammability traits differ between families and growth forms?
+##Is flammability (can be done per flammatrait) explained by family or  growth form?
+summary(lm(value ~ Accepted_family, data = CombinedLong, subset = (flammtrait == "FlammabilityIndex")))
+
+# Model with growth form
+summary(lm(value ~ growth_form, data = CombinedLong, subset = (flammtrait == "FlammabilityIndex")))
+                                                                              #familiy explain a little more flamm than growth form
+
+
+#which families are more flammable? PCA maybe?
+#i run this on wide data :(
+pca_res <- prcomp(CombinedOG[, c("TimeToFlaming(s)", "MaximumFlameTemperature(°C)", "PostBurntMassEstimate(%)")], 
+                  scale. = TRUE)
+
+#install.packages('ggfortify')
+autoplot(pca_res, data = CombinedOG, colour = "Accepted_family", label = FALSE) +
+  theme_minimal()     #way too colourful...perhaps try medians rather? 
+
+
+fviz_pca_var(pca_res, col.var = "contrib", repel = TRUE)
+
 
 # structure overview of the entire dataframe
 str(CombinedLong)
@@ -122,11 +172,13 @@ CombinedOG%>%
   select(`Date`, `MaximumFlameTemperature(°C)`,`FMC(%)`) %>%
   arrange(desc(`MaximumFlameTemperature(°C)`))
 
+
 # plot everything against everything
 CombinedOG %>%
   select(`Site`, `TimeToFlaming(s)`, `PostBurntMassEstimate(%)`, `MaximumFlameTemperature(°C)`, `FMC(%)`) %>%
     GGally::ggpairs(columns = 2:ncol(.))
 
+#or
 
 # Select only the numeric flammability columns for pairwise plots
 CombinedOG %>%
@@ -139,14 +191,47 @@ CombinedOG %>%
   ggpairs()         
                                 
        
-#maximum temperature vs postburntmass
-ggplot(CombinedOG, aes(x = `MaximumFlameTemperature(°C)`, y = `PostBurntMassEstimate(%)`)) +
-  geom_point(alpha = 0.6) +
-  geom_smooth(method = "lm", se = TRUE, color = "blue") +
+#trait distribution across sites with violin plots
+ggplot(CombinedLong, aes(x = Site, y = value, fill = flammtrait)) +
+  geom_violin(trim = FALSE, alpha = 0.6) +
+  facet_wrap(~ flammtrait, scales = "free_y") +
+  theme_minimal()
+
+
+#Is the timing of collection (AM vs PM) influencing trait values?
+# Does it matter when we collect relative to when we burn?
+
+#whats on these two...
+unique(CombinedLong$`time collected`)
+unique(CombinedLong$BurnSession)
+
+#
+ggplot(filter (CombinedLong, flammtrait != "FlammabilityIndex"), aes(x = `time collected`, y = value, fill = `time collected`)) +
+  geom_boxplot() +
+  facet_wrap(~ flammtrait, scales = "free_y") +
+  theme_minimal() +
   labs(title = "",
-       x = "Maximum Temperature (°C)",
-       y = "Unburned Plant Mass") +
-  theme_minimal()                             #mmh...
+       x = "", y = "")
+                                                                #interesting minimal spread of values in PM collection
+
+
+#Does TimeCollected (AM/PM), BurntSession affect trait value?
+ggplot(filter (CombinedLong, flammtrait != "FlammabilityIndex"), aes(x = BurnSession, y = value, fill = `time collected`)) +
+  geom_boxplot(position = position_dodge()) +
+  facet_wrap(~ flammtrait, scales = "free_y") +
+  theme_minimal() +
+  labs(title = "",
+       x = "Burn session", y = "", fill = "Time Collected")
+                  #
+
+#test
+model01 <- lm(value ~ `time collected` * BurnSession, data = CombinedLong)
+summary(model01)
+
+
+
+
+
 
 
 
