e7c07be52b47dc3805bd2d1092c5c82a7208e9a0
Samukelisiwe Msweli <samukelisiwem@mandela.ac.za>
2025-11-04 15:31:19 +0200

    updates

--- JoinedFlamm_Trait/Script/20251104_.R
+++ JoinedFlamm_Trait/Script/20251104_.R
@@ -28,10 +28,44 @@ unique(Fieldtrait$scientific_name_WFO)  #1000+ species
 
 unique(grootbos$`scientific name`)
 
-#create df of sharedspecies_grot
-grootshared <- intersect(unique(Fieldtrait$scientific_name_WFO), 
-                            unique(grootbos$`scientific name`))
+#see shared species
 
+grootshared <- intersect(unique(Fieldtrait$scientific_name_WFO), 
+          unique(grootbos$`scientific name`)) #183spp
+
+length(grootshared)   
 print(grootshared)
 
+#filter shared species from each
+grootbos_shared <- grootbos %>%
+  filter(`scientific name` %in% grootshared)
+
+Fieldtrait_shared <- Fieldtrait %>%
+  filter(scientific_name_WFO %in% grootshared)
+
+
+#join
+groot_final <- left_join(
+  grootbos_shared,
+  Fieldtrait_shared,
+  by = c("scientific name" = "scientific_name_WFO")
+)
+
+unique(groot_final$`scientific name`)
+
+Grootspecies_list <- data.frame(scientific_name = unique(groot_final$`scientific name`))
+head(Grootspecies_list )
+nrow(Grootspecies_list )   # should be 183
+
+unique(groot_final$LF_GandP)
+
+groot_final %>%
+  count(LF_GandP, sort = TRUE)
+
+
+
+#save
+#grootspeciesList <- data.frame(scientific_name = groot_final)
+
+writexl::write_xlsx(Grootspecies_list, "Data/Grootspecies_list.xlsx")
 
